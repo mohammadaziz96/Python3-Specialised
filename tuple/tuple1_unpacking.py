cb8d12e0c0afe596e610d0886b66a9efbc6acc65
Mohammad Aziz <aziznitham@gmail.com>
2020-05-14 12:06:26 +0530

    Add files via upload

---
+++ tuple/tuple1_unpacking.py
@@ -0,0 +1,7 @@
+"""4. Provided is a list of tuples. Create another list called t_check that contains the third element of every tuple."""
+
+lst_tups = [('Articuno', 'Moltres', 'Zaptos'), ('Beedrill', 'Metapod', 'Charizard', 'Venasaur', 'Squirtle'), ('Oddish', 'Poliwag', 'Diglett', 'Bellsprout'), ('Ponyta', "Farfetch'd", "Tauros", 'Dragonite'), ('Hoothoot', 'Chikorita', 'Lanturn', 'Flaaffy', 'Unown', 'Teddiursa', 'Phanpy'), ('Loudred', 'Volbeat', 'Wailord', 'Seviper', 'Sealeo')]
+t_check=[]
+for i in lst_tups:
+    t_check.append(i[2])
+print(t_check)

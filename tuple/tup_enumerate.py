fb4f73a4dd4fc282966515c20f40ecf8d3cbc926
Mohammad Aziz <aziznitham@gmail.com>
2020-05-14 12:10:33 +0530

    Add files via upload

---
+++ tuple/tup_enumerate.py
@@ -0,0 +1,3 @@
+fruits = ['apple', 'pear', 'apricot', 'cherry', 'peach']
+for i in enumerate(fruits):   #enumerate returns tuple but first would be sequence of numbers and 2nd would be element from original list.
+    print(i[0], i[1])

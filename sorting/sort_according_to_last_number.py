d06950dde1be6000bfd6d7b5c5050b2be2a62fa8
Mohammad Aziz <aziznitham@gmail.com>
2020-05-18 12:25:20 +0530

    Add files via upload

---
+++ sorting/sort_according_to_last_number.py
@@ -0,0 +1,8 @@
+"""Below, we have provided a list of strings called nums. Write a function called last_char that takes a string as input, and returns only its last character. Use this function to sort the list nums by the last digit of each number, from highest to lowest, and save this as a new list called nums_sorted."""
+
+nums = ['1450', '33', '871', '19', '14378', '32', '1005', '44', '8907', '16']
+
+def last_char(s):
+    return s[-1]
+nums_sorted = sorted(nums, key=last_char, reverse=True)
+print(nums_sorted)

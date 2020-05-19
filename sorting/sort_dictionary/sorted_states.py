dfebc6d105923ca3c615941156b5e1cd42663527
Mohammad Aziz <aziznitham@gmail.com>
2020-05-19 10:03:47 +0530

    Add files via upload

---
+++ sorting/sort_dictionary/sorted_states.py
@@ -0,0 +1,7 @@
+# sort states according to length of their first city.
+
+states = {"Minnesota": ["St. Paul", "Minneapolis", "Saint Cloud", "Stillwater"],
+          "Michigan": ["Ann Arbor", "Traverse City", "Lansing", "Kalamazoo"],
+          "Washington": ["Seattle", "Tacoma", "Olympia", "Vancouver"]}
+
+print(sorted(states, key=lambda state: len(states[state][0])))

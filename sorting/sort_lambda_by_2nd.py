6d063d9b2735c86a14a80490a664003ce611e745
Mohammad Aziz <aziznitham@gmail.com>
2020-05-19 10:55:51 +0530

    Delete sort_lambda_by_2nd.py

--- sorting/sort_lambda_by_2nd.py
+++
@@ -1,4 +0,0 @@
-ex_lst = ['hi', 'how are you', 'bye', 'apple', 'zebra', 'dance']
-lambda_sort=sorted(ex_lst, key=lambda x: str(x)[1])
-#key will return 2nd element and accoding to 2nd letter ex_let would be sorted
-print(lambda_sort)

bc6c1165cd8af37554b652aebb4bc9b557af3c75
gambler2020 <aziznitham@gmail.com>
2023-12-13 18:53:11 +0000

    modify variables outside class

--- OOPS/class_and_object.py
+++ OOPS/class_and_object.py
@@ -1,4 +1,9 @@
 class Ansari:
  owner_name=""
  kids=0
+ print(f'{kids}')
 family=Ansari()
+family.kids=6 #modify variables here. no need to chage in class
+family1=Ansari()
+print(f'Number of kids: {family.kids}')
+print(f'{family1.kids}')
